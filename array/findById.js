325fbe35f0171852b70db94d7f3c9b6aae7b9bb5
ath08 <0807ath@gmail.com>
2020-08-27 00:29:27 +0400

    Create findById.js file

---
+++ array/findById.js
@@ -0,0 +1,21 @@
+/*
+    Write a function which finds corresponding object by it’s id and print it. Elsewhere it
+    should print error message.
+*/
+
+const findById = (arr, Id) => {
+    for (let item of arr) {
+      if (item["id"] === Id) return item;
+    }
+  
+    return `Not found`;
+  };
+  
+  let arr = [
+    { id: 1, title: "react" },
+    { id: 2, title: "angular" },
+    { id: 3, title: "vue" },
+    { id: 4, title: "node" },
+  ];
+
+  findById(arr, 2);
